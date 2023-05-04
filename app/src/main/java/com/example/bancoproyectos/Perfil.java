011be761d8d85f310ff8186f4cb8d631ecb8ec59
Andrescruzxr <andrecruzx26@gmail.com>
2023-05-03 21:07:04 -0500

    andres commit

--- app/src/main/java/com/example/bancoproyectos/Perfil.java
+++ app/src/main/java/com/example/bancoproyectos/Perfil.java
@@ -2,7 +2,9 @@ package com.example.bancoproyectos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Perfil extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class Perfil extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
+
+
+    }
+
+    public void click(View view) {
+        Intent click = new Intent(this, sppiner.class);
+        startActivity(click);
     }
 }
--- app/src/main/java/com/example/bancoproyectos/sppiner.java
+++ app/src/main/java/com/example/bancoproyectos/sppiner.java
@@ -2,8 +2,10 @@ package com.example.bancoproyectos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
@@ -12,29 +14,55 @@ import java.util.ArrayList;
 public class sppiner extends AppCompatActivity {
 
     Spinner spinner;
+    private boolean isFirstSelection = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sppiner);
-        spinner = (Spinner)findViewById(R.id.spinner);
+        spinner = (Spinner) findViewById(R.id.spinner);
 
-       String  [] respuestas = {"perfil","mis proyectos","equipo de trabajo"};
-        ArrayAdapter <String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,respuestas);
+        String[] respuestas = {"perfil", "mis proyectos", "equipo de trabajo",};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, respuestas);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
-
-    }
-    public void seleccionado (View view){
-        String seleccionado = spinner.getSelectedItem().toString();
-        if (seleccionado.equals("perfil")){
-        /*intent seleccionado = new intent(this,Perfil.class);
-        startActivity(seleccionado);]*/
+        spinner.setSelection(0);
+        /**spinner.setSelection(adapter.getPosition(""));*/
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
 
 
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                if (isFirstSelection) {
+                    enviarAccion(position);
+                } else {
+                    isFirstSelection = true;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
         }
-        else if (seleccionado.equals("mis proyectos")){
+    public void enviarAccion(int position) {
+        boolean spinnerEnabled = false;
+        String selectedItem = spinner.getItemAtPosition(position).toString();
 
-        } else if (seleccionado.equals("equipo de trabajo")) {
-
+        if (selectedItem.equals("perfil")) {
+            Intent mostrar = new Intent(this, Perfil.class);
+            startActivity(mostrar);
+        } else if (selectedItem.equals("mis proyectos")) {
+            Intent mostrar = new Intent(this, MisProyectos.class);
+            startActivity(mostrar);
+        } else if (selectedItem.equals("equipo de trabajo")) {
+            Intent mostrar = new Intent(this, EquipoTrabajo.class);
+            startActivity(mostrar);
         }
     }
+
 }
+
+
