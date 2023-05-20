c4bfe64fe3abf5190b12067b85dbad1baceb8d6d
andresrengifo2002 <raulrentellito@gmail.com>
2023-05-20 09:37:04 -0500

    andres21 commit

--- app/src/main/java/com/example/bancoproyectos/MenuPrincipal.java
+++ app/src/main/java/com/example/bancoproyectos/MenuPrincipal.java
@@ -1,12 +1,18 @@
 package com.example.bancoproyectos;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
-import androidx.appcompat.widget.Toolbar;
+import android.view.MenuItem;
 
+
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 
 import com.google.android.material.navigation.NavigationView;
@@ -15,7 +21,10 @@ public class MenuPrincipal extends AppCompatActivity {
     DrawerLayout drawerLayout;
     ActionBarDrawerToggle actionBarDrawerToggle;
     Toolbar toolbar;
-    NavigationView navigationView;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +35,12 @@ public class MenuPrincipal extends AppCompatActivity {
         toolbar = findViewById(R.id.toolbar);
 
         drawerLayout = findViewById(R.id.drawer);
-        navigationView = findViewById(R.id.navigationView);
 
-        actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
         actionBarDrawerToggle.syncState();
+
+
     }
 }
