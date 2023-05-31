f6f70647c14ab745a3f3153a59da132cfbb14725
andresrengifo2002 <raulrentellito@gmail.com>
2023-05-31 10:46:10 -0500

    second commit

--- app/src/main/java/com/example/bancoproyectos/MisProyectos.java
+++ app/src/main/java/com/example/bancoproyectos/MisProyectos.java
@@ -2,7 +2,9 @@ package com.example.bancoproyectos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MisProyectos extends AppCompatActivity {
 
@@ -11,4 +13,11 @@ public class MisProyectos extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mis_proyectos);
     }
+    public void onClick(View view) {
+
+
+        Intent intent = new Intent(this, MenuPrincipal.class);
+        startActivity(intent);
+    }
+
 }
