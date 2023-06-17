16c20927df11f0b56afd7736e8f01923e1193ca1
andresrengifo2002 <raulrentellito@gmail.com>
2023-06-17 09:42:19 -0500

    second123 commit

--- app/src/main/java/com/example/bancoproyectos/iComunicaFragments.java
+++ app/src/main/java/com/example/bancoproyectos/iComunicaFragments.java
@@ -3,7 +3,5 @@ package com.example.bancoproyectos;
 import com.example.bancoproyectos.Proyectos;
 
 public interface iComunicaFragments {
-    //esta interface se encarga de realizar la comunicacion entre la lista de personas y el detalle
-    public void enviarPersona(Proyectos proyectos); //se transportara un objeto de tipo persona
-    //(En la clase Persona se implementa Serializable para poder transportar un objeteo a otro)
+    public void enviarPersona(Proyectos proyectos);
 }
